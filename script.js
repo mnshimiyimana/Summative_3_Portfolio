7ea66a24906560e045c966549d5bf65f051d39f5
clever-cr <cleverumuhuza@gmail.com>
2024-04-17 22:53:24 +0200

    add few changes

--- script.js
+++ script.js
@@ -43,11 +43,15 @@ $(document).ready(function () {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 300, function(){
-        window.location.hash = hash;
-      });
-    } 
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        100,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
 });
